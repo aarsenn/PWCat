cd461a2d5858d402a062fb078e642bf65c16636d
Arsen Terletsky <arsen@Arsens-MacBook-Pro.local>
2018-07-03 23:36:32 +0300

    fix SW

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { MatToolbarModule, MatCardModule, MatButtonModule } from '@angular/material';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { ImgCardComponent } from './img-card/img-card.component';
@@ -15,7 +17,8 @@ import { ImgCardComponent } from './img-card/img-card.component';
     BrowserModule,
     MatToolbarModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
   ],
   providers: [],
   bootstrap: [AppComponent]
